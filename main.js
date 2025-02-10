524ddf92695c4f2ee2d8ead2b3aac299cb54399d
molenaru <121340870+molenaru@users.noreply.github.com>
2025-02-10 19:40:29 +0700

    Update JS

--- main.js
+++ main.js
@@ -1,18 +1,31 @@
-	var display = document.getElementById('display');
-		function button	(obj) {
-			var kondisi = obj.innerHTML;
-			if (kondisi == "=") {
-				display.innerHTML = eval(display.innerHTML);
-			} else if (kondisi == "C") {
-				display.innerHTML = "";
-			} else if (kondisi == "Del") {
-				display.innerHTML = display.innerHTML.slice(0,-1);
-			} else {
-				if (display.innerHTML == "") {
-					display.innerHTML = kondisi;
-				} else {
-					display.innerHTML += kondisi;
-				}
-			}
-		}	
-		
+let display = document.getElementById('display');
+function button(obj) {
+	let kondisi = obj.innerHTML;
+	if (kondisi == "=") {
+		display.innerHTML = eval(display.innerHTML);
+	} else if (kondisi == "C") {
+		display.innerHTML = "";
+	} else if (kondisi == "Del") {
+		display.innerHTML = display.innerHTML.slice(0, -1);
+	} else {
+		if (display.innerHTML == "") {
+			display.innerHTML = kondisi;
+		} else {
+			display.innerHTML += kondisi;
+		}
+	}
+}
+
+
+function gantiMode(pil) {
+	let mode = document.querySelector("link");
+	mode.getAttribute("href");
+	let tombol = pil.innerHTML;
+	if (tombol == "Dark") {
+		mode.setAttribute("href", "dark_mode.css");
+	} else if (tombol == "Neon") {
+		mode.setAttribute("href", "neon_mode.css");
+	} else {
+		mode.setAttribute("href", "styles.css");
+	}
+}
